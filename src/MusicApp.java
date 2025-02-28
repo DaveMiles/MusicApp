bff2e9a27a71d8124cd84052fe730adff3d65233
david.miles2 <david.miles2@sky.uk>
2025-02-28 13:39:20 +0000

    init: project creation

---
+++ src/MusicApp.java
@@ -0,0 +1,17 @@
+/*
+Your task is to develop a basic Java console application that will emulate the main features of a music
+streaming service such as Spotify or Apple Music. 
+You should code a basic application that is able to store a list of song objects. Each song object should be
+able to store the artist’s name, the song title, and the current number of times the song has been played.
+Your application should have the following features:
+1. Add a new song to the list of songs
+2. Remove a song from the list of songs
+3. Print a list of all the songs stored
+4. Print a list of songs over a given number of plays 
+*/
+
+public class MusicApp {
+  public static void main(String[] args) {
+
+  }
+}
