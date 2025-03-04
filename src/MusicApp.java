60b409b1aba851689b3e2a58d5094ab1b24d2156
david.miles2 <david.miles2@sky.uk>
2025-03-04 19:11:55 +0000

    feat: add print over a number of plays and add new song feature

--- src/MusicApp.java
+++ src/MusicApp.java
@@ -16,16 +16,18 @@ import java.util.Scanner;
 class Song {
   private String artist;
   private String title;
-  private int plays;
+  public int plays;
+  private int id;
 
-  public Song(String artist, String title, int plays) {
+  public Song(String artist, String title, int plays, int id) {
     this.artist = artist;
     this.title = title;
     this.plays = plays;
+    this.id = id;
   }
 
   public void print() {
-    System.out.printf("Artist: %-15s | Title: %-20s | Plays: %,15d%n", artist, title, plays);
+    System.out.printf("ID: %-2d | Artist: %-15s | Title: %-20s | Plays: %,15d%n", id, artist, title, plays);
   }
 }
 
@@ -35,22 +37,38 @@ class SongList {
   public SongList() {
     songs = new ArrayList<Song>();
 
-    songs.add(new Song("Seafret", "Atlantis", 1041721661));
-    songs.add(new Song("Seafret", "Oceans", 202311668));
-    songs.add(new Song("Seafret", "Wildfire", 59731592));
-    songs.add(new Song("Orla Gartland", "Why Am I Like This?", 76399312));
-    songs.add(new Song("Orla Gartland", "Little Chaos", 3220324));
-    songs.add(new Song("Orla Gartland", "More Like You", 19187743));
-    songs.add(new Song("Gigi Perez", "Sailor Song", 758039084));
-    songs.add(new Song("Chappel Roan", "Pink Pony Club", 1312588412));
-    songs.add(new Song("Chappel Roan", "HOT TO GO!", 595970756));
-    songs.add(new Song("E^ST", "Friends", 16514295));
+    songs.add(new Song("Seafret", "Atlantis", 1041721661, 1));
+    songs.add(new Song("Seafret", "Oceans", 202311668, 2));
+    songs.add(new Song("Seafret", "Wildfire", 59731592, 3));
+    songs.add(new Song("Orla Gartland", "Why Am I Like This?", 76399312, 4));
+    songs.add(new Song("Orla Gartland", "Little Chaos", 3220324, 5));
+    songs.add(new Song("Orla Gartland", "More Like You", 19187743, 6));
+    songs.add(new Song("Gigi Perez", "Sailor Song", 758039084, 7));
+    songs.add(new Song("Chappel Roan", "Pink Pony Club", 1312588412, 8));
+    songs.add(new Song("Chappel Roan", "HOT TO GO!", 595970756, 9));
+    songs.add(new Song("E^ST", "Friends", 16514295, 10));
   }
 
   public void printAllSongs() {
     for (Song song : songs) {
       song.print();
     }
+  }
+
+  public void printSongsOverPlays(int minPlays) {
+    for (Song song : songs) {
+      if (song.plays > minPlays) {
+        song.print();
+      }
+    }
+  }
+
+  public int getListLength() {
+    return songs.size();
+  }
+
+  public void addSong(String artist, String title, int plays, int id) {
+    songs.add(new Song(artist, title, plays, id));
   }
 }
 
@@ -61,7 +79,7 @@ public class MusicApp {
       SongList songList = new SongList();
 
       while (programRunning) {
-        System.out.println("");
+        addNewlineToConsole();
         System.out.println("----------");
         System.out.println("Enter a number to select an option:");
         System.out.println("1. Print all songs");
@@ -73,7 +91,8 @@ public class MusicApp {
 
         int input = reader.nextInt();
         reader.nextLine();
-        System.out.println("");
+
+        addNewlineToConsole();
 
         switch (input) {
           case 1:
@@ -82,9 +101,11 @@ public class MusicApp {
             break;
           case 2:
             // Print songs over a given number of plays
+            printSongsOverPlays(reader, songList);
             break;
           case 3:
             // Add a new song to the list of songs
+            addSongToSongList(reader, songList);
             break;
           case 4:
             // Remove a song from the list of songs
@@ -98,5 +119,40 @@ public class MusicApp {
       }
 
     }
+  }
+
+  private static void addSongToSongList(Scanner reader, SongList songList) {
+    addNewlineToConsole();
+    System.out.println("Enter the artist's name:");
+    String artist = reader.nextLine();
+    addNewlineToConsole();
+    System.out.println("Enter the song title:");
+    String title = reader.nextLine();
+    addNewlineToConsole();
+    System.out.println("Enter the number of plays:");
+    int plays = reader.nextInt();
+    reader.nextLine();
+    addNewlineToConsole();
+    System.out.println("Song added to the list:");
+    addNewlineToConsole();
+    int id = songList.getListLength() + 1;
+    songList.addSong(artist, title, plays, id);
+
+  }
+
+  private static void printSongsOverPlays(Scanner reader, SongList songList) {
+    addNewlineToConsole();
+    System.out.println("Enter the minimum number of plays:");
+    int minPlays = reader.nextInt();
+    reader.nextLine();
+    addNewlineToConsole();
+    System.out.println("Songs with more than " + String.format("%,d", minPlays) + " plays:");
+    addNewlineToConsole();
+    songList.printSongsOverPlays(minPlays);
+  }
+
+  // adds a new line to console to make it look cleaner
+  private static void addNewlineToConsole() {
+    System.out.println("");
   }
 }
